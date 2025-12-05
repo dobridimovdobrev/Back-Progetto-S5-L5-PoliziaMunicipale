f1b1c276a3bb714c67792295c483b3bf8d9b3f15
Dobri Dimov Dobrev <126101328+dobridimovdobrev@users.noreply.github.com>
2025-12-05 12:16:50 +0100

    Aggiungere i file di progetto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Back_Progetto_S5_L5_PoliziaMunicipale.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
